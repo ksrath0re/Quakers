dfbcdc2dc61cd17cb257fae21281e4bb0a051205
krathor <kuldeepsinghlive@gmail.com>
2019-03-28 16:37:13 -0700

    Code for generating aggregated data for regions with different times

--- region_collate.py
+++ region_collate.py
@@ -21,7 +21,7 @@ r.extend(region_1)
 r.extend(region_2)
 r.extend(region_3)
 r.extend(region_4)
-print(r)
+#print(r)
 #print(os.listdir(dir))
 # for folder in os.listdir(dir):
 #     for x in regions.keys():
@@ -30,33 +30,48 @@ region_1_data = []
 region_2_data = []
 region_3_data = []
 region_4_data = []
+region_1_data_new = []
+region_2_data_new = []
+region_3_data_new = []
+region_4_data_new = []
+
 for root, dirs, files in os.walk(dir):
     #print('Root :',root,' dirs : ',dirs, ' files :',files)
 
     if dirs and dirs[0] == 'CB-Results':
+        print('Root :', root, ' dirs : ', dirs, ' files :', files)
         plant_name = root.split('\\')[-1:][0]
+        year = root.split('\\')[-2]
         if plant_name in region_1:
-            region_1_data.append(returnDataFrame(root, plant_name))
-            print("Plant Name for Region 1: ", plant_name)
+            region_1_data.append(returnDataFrame(root, plant_name)) if year == '1998-2004 CSV plant wise' else region_1_data_new.append(returnDataFrame(root, plant_name))
+            #print("Plant Name for Region 1: ", plant_name)
         elif plant_name in region_2:
-            region_2_data.append(returnDataFrame(root, plant_name))
+            region_2_data.append(returnDataFrame(root, plant_name)) if year == '1998-2004 CSV plant wise' else region_2_data_new.append(returnDataFrame(root, plant_name))
             #print("Plant Name for Region 2: ", plant_name)
         elif plant_name in region_3:
-            region_3_data.append(returnDataFrame(root, plant_name))
+            region_3_data.append(returnDataFrame(root, plant_name)) if year == '1998-2004 CSV plant wise' else region_3_data_new.append(returnDataFrame(root, plant_name))
             #print("Plant Name for Region 3: ", plant_name)
         else:
-            region_4_data.append(returnDataFrame(root, plant_name))
+            region_4_data.append(returnDataFrame(root, plant_name)) if year == '1998-2004 CSV plant wise' else region_4_data_new.append(returnDataFrame(root, plant_name))
             #print("Plant Name for Region 4: ", plant_name)
 
 region_1_file = pd.concat(region_1_data)
 region_2_file = pd.concat(region_2_data)
 region_3_file = pd.concat(region_3_data)
 region_4_file = pd.concat(region_4_data)
+region_1_file_new = pd.concat(region_1_data_new)
+region_2_file_new = pd.concat(region_2_data_new)
+region_3_file_new = pd.concat(region_3_data_new)
+region_4_file_new = pd.concat(region_4_data_new)
 
-region_1_file.to_csv(os.path.join(dir, "region_1.csv"))
-region_2_file.to_csv(os.path.join(dir, "region_2.csv"))
-region_3_file.to_csv(os.path.join(dir, "region_3.csv"))
-region_4_file.to_csv(os.path.join(dir, "region_4.csv"))
+region_1_file.to_csv(os.path.join(dir, "region_1_old.csv"))
+region_2_file.to_csv(os.path.join(dir, "region_2_old.csv"))
+region_3_file.to_csv(os.path.join(dir, "region_3_old.csv"))
+region_4_file.to_csv(os.path.join(dir, "region_4_old.csv"))
+region_1_file_new.to_csv(os.path.join(dir, "region_1_new.csv"))
+region_2_file_new.to_csv(os.path.join(dir, "region_2_new.csv"))
+region_3_file_new.to_csv(os.path.join(dir, "region_3_new.csv"))
+region_4_file_new.to_csv(os.path.join(dir, "region_4_new.csv"))
 
 
 
