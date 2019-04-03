5f155a7769e9e7ee9b9354591e74cf73bc28eabb
krathor <kuldeepsinghlive@gmail.com>
2019-04-02 21:22:44 -0700

    Code for creating network graphs for all regions with two timeperiods

--- plant_network.py
+++ plant_network.py
@@ -7,11 +7,26 @@ import json
 import os
 import os.path
 
-def getNodes(filename):
+def getEdges(filename):
     with open(filename, 'r') as nodecsv:
         nodereader = csv.reader(nodecsv)
         edges = [tuple(e[2:]) for e in nodereader][1:]
         return edges
+
+def getAggregatedEdgesAndRoles(edges):
+    dic = {}
+    roles = set()
+    for x in edges:
+        if (x[0], x[1]) in dic.keys():
+            dic[(x[0], x[1])] = int(x[2])+ int(dic[(x[0], x[1])])
+        else:
+            dic[(x[0], x[1])] = int(x[2])
+        roles.add(x[0])
+        roles.add(x[1])
+    aggregatedEdges = []
+    for k, v in dic.items():
+        aggregatedEdges.append((k[0], k[1], v))
+    return aggregatedEdges , roles
 
 dir = 'I:\Data Science Lab\LERs Data for Network Construction'
 files = {
@@ -26,20 +41,26 @@ files = {
 
 }
 
-roles = set()
-temp = []
-edges = []
+region_edges = {}
+roles_set = set()
 for k,v in files.items():
+    region_file_name = k + '_edges'
     file_name = os.path.join(dir,v)
-    edges = getNodes(file_name)
-    #temp.append(x[0],x[1] for x in edges)
-    for x in edges:
-        roles.add(x[0])
-        roles.add(x[1])
-roles = list(roles)
+    edges = getEdges(file_name)
+    aggregatedEdges, roles = getAggregatedEdgesAndRoles(edges)
+    region_edges[region_file_name] = aggregatedEdges
+    roles_set |= roles
 
 
+graphs = {}
+for k, v in region_edges.items():
+    k  = k.replace('_edges','')
+    graph = nx.Graph()
+    graph.add_nodes_from(roles_set)
+    graph.add_weighted_edges_from(v)
+    print("for Graph ", k, nx.info(graph))
+    print('---------------------------------------------')
+    graphs[k] = graph
 
-graph = nx.Graph()
-graph.add_nodes_from(roles)
-print(nx.info(graph))
+
+print(graphs)
